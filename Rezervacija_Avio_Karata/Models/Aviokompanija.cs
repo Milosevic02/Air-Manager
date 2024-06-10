33612c976514aadd4f1ee4070e395e75c710a87c
Dragan <milosevicdragan002@gmail.com>
2024-06-10 20:59:05 +0200

    Create Aviokompanija.cs

---
+++ Rezervacija_Avio_Karata/Models/Aviokompanija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rezervacija_Avio_Karata.Models
+{
+    public class Aviokompanija
+    {
+        public string Naziv {  get; set; }
+        public string Adresa {  get; set; }
+        public string Kontakt_informacije {  get; set; }
+        public List<Let>Letovi { get; set; }
+        public List<Recenzija> Recenzije { get;set; }
+
+        public Aviokompanija() { }  
+
+
+    }
+}
