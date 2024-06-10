18e5a4a946ff531ffc252c2d0f60534849879646
Dragan <milosevicdragan002@gmail.com>
2024-06-10 21:04:13 +0200

    Create Let.cs

---
+++ Rezervacija_Avio_Karata/Models/Let.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rezervacija_Avio_Karata.Models
+{
+    public enum Status
+    {
+        Aktivan,
+        Otkazan,
+        Zavrsen
+    }
+    public class Let
+    {
+        public Aviokompanija Aviokompanija { get; set; }
+        public string PolaznaDestinacija {  get; set; }
+        public string OdredisnaDestinacija { get; set; }
+        public DateTime DatumVremePolaska {  get; set; }
+        public DateTime DatumVremeDolaska { get; set; }
+        public int BrojSlobodnihMesta {  get; set; }
+        public int BrojZauzetihMesta { get; set; }
+        public double Cena {  get; set; }
+        public Status Status { get; set; }
+
+
+
+    }
+}
