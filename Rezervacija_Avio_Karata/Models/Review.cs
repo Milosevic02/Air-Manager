c4374999d9151178a476a6c87cf3bd4d5bb95eea
Dragan <milosevicdragan002@gmail.com>
2024-06-11 20:57:14 +0200

    Create Review.cs

---
+++ Rezervacija_Avio_Karata/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rezervacija_Avio_Karata.Models
+{
+    public enum ReviewStatus
+    {
+        Created,
+        Approved,
+        Rejected
+    }
+    public class Review
+    {
+        public User Reviewer { get; set; }
+        public Airline Airline { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image {  get; set; } = String.Empty;
+        public ReviewStatus ReviewStatus { get; set; }
+    }
+}
