12b0f000d00bc90ae5a059c346b31a0835f70bed
Dragan <milosevicdragan002@gmail.com>
2024-06-11 20:50:35 +0200

    Create Flight.cs

---
+++ Rezervacija_Avio_Karata/Models/Flight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rezervacija_Avio_Karata.Models
+{
+    public enum FlightStatus
+    {
+        Active,
+        Cancelled,
+        Completed
+    }
+    public class Flight
+    {
+        public Airline Airline { get; set; }
+        public string DepartureDestination { get; set; }
+        public string ArrivalDestination { get; set; }
+        public DateTime DepartureDateAndTime { get; set; }
+        public DateTime ArrivalDateAndTime { get; set; }
+        public int AvailableSeats { get; set; }
+        public int OccupiedSeats { get; set; }
+        public double Price { get; set; }
+        public FlightStatus FlightStatus {get;set;}
+    }
+}
