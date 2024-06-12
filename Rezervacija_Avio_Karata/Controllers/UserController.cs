cdaaca5df9579faa761ede4f0f1f5da7a3778933
Dragan <milosevicdragan002@gmail.com>
2024-06-12 21:16:36 +0200

    RegisterUser func

---
+++ Rezervacija_Avio_Karata/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Rezervacija_Avio_Karata.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Rezervacija_Avio_Karata.Controllers
+{
+    [RoutePrefix("api")]
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        [Route("RegisterUser")]
+        public IHttpActionResult RegisterUser(User user)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Users.txt"));
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(content) ?? new List<User>();
+            var exsists = users.Find(x=> x.Username == user.Username);
+            if (exsists != null)
+            {
+                return BadRequest();
+            }
+            
+            string[] dateTime = user.DateOfBirth.Split(' ');
+            user.DateOfBirth = dateTime[0];
+
+            users.Add(user);
+            content = JsonConvert.SerializeObject(users, Formatting.Indented);
+            File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Users.txt"), content);
+            return Ok();
+        }
+    }
+}
