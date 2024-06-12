2075dc6aab6fe0982e7a6a3aa563e50063ff6947
Dragan <milosevicdragan002@gmail.com>
2024-06-12 21:30:04 +0200

    Change Date to string

--- Rezervacija_Avio_Karata/Models/User.cs
+++ Rezervacija_Avio_Karata/Models/User.cs
@@ -17,7 +17,7 @@ namespace Rezervacija_Avio_Karata.Models
         public string Email { get; set; }
         public string Name { get; set; }    
         public string Surname {  get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string DateOfBirth { get; set; }
         public Gender Gender { get; set; }
         public List<Reservation> Reservations { get; set; } = new List<Reservation>();
 
