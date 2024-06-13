714808d47479638097c08cb6e278c12b6620c088
Dragan <milosevicdragan002@gmail.com>
2024-06-13 15:25:04 +0200

    Add login func

--- Rezervacija_Avio_Karata/Pages/Script/Login.js
+++ Rezervacija_Avio_Karata/Pages/Script/Login.js
@@ -1,3 +1,28 @@
+function Login(){
+    let form = $("#loginForm")
+    let data = convertFormToJSON(form)
+
+    $.ajax({
+        url:"/api/LoginUser",
+        type:"GET",
+        data:{username:data.username,password:data.password},
+        contentType:"text/plain",
+        complete: function(result){
+            if(result.status == 400){
+                console.log("Error when log in")
+            }else if(result == 200){
+                console.log("Success Log in")
+                window.location.href = "Index.html"
+            }else{
+                console.log("Unknown error when log in")
+            }
+        }
+    })
+
+}
+
+
+
 function convertFormToJSON(form) {
     const array = $(form).serializeArray(); // Encodes the set of form elements as an array of names and values.
     const json = {};
