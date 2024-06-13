d6dd17b96940ff02cbf1bf6ce33a9d9a88a7f27c
Dragan <milosevicdragan002@gmail.com>
2024-06-13 15:26:00 +0200

    LoginUser func

--- Rezervacija_Avio_Karata/Controllers/UserController.cs
+++ Rezervacija_Avio_Karata/Controllers/UserController.cs
@@ -34,5 +34,36 @@ namespace Rezervacija_Avio_Karata.Controllers
             File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Users.txt"), content);
             return Ok();
         }
+
+        [HttpGet]
+        [Route("LoginUser")]
+        public IHttpActionResult LoginUser(string username, string password)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Users.txt"));
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(content) ?? new List<User>();
+            var exsists = users.Find(x => x.Username.Equals(username));
+            if (exsists == null)
+            {
+                return BadRequest("User with username \"" + username + "\" doesn't exist!");
+
+            }
+
+            if (exsists.Password != password)
+            {
+                return BadRequest("Incorrect password");
+            }
+
+            User current = (User)HttpContext.Current.Session["user"];
+            if (current != null && current.Username == username)
+            {
+                return BadRequest("User already logged in");
+            }
+
+            HttpContext.Current.Session["user"] = exsists;
+            return Ok(username + " successfully logged in");
+
+        }
     }
+
+
 }
