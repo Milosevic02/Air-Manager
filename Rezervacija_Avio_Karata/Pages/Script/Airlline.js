8107c4be481f1ff0b9c2ccfbbf46df697cb8b664
Dragan <milosevicdragan002@gmail.com>
2024-06-14 21:30:53 +0200

    Update Airlline.js

--- Rezervacija_Avio_Karata/Pages/Script/Airlline.js
+++ Rezervacija_Avio_Karata/Pages/Script/Airlline.js
@@ -1,7 +1,6 @@
 function LoadAirllines(){
     $.get("/api/GetAllAirllines",function(data){
-        let table = '<table class="table table-striped table-hover table-bordered fs-5">';
-        table += '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Address</th><th scope="col">Contact Info</th><th scope="col">Edit</th><th scope="col">Delete</th></tr></thead><tbody>';
+        let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Address</th><th scope="col">Contact Info</th><th scope="col">Edit</th><th scope="col">Delete</th></tr></thead><tbody>';
 
         let counter = 0;
         for(airlline in data){
@@ -19,4 +18,37 @@ function LoadAirllines(){
         table += '</tbody></table>';
         $('#airllineTable').html(table);
     })
+}
+
+function AddAirlline(event){
+    event.preventDefault(); 
+
+    let form = $("#addAirlineForm");
+    let data = convertFormToJSON(form);
+    
+    $.ajax({
+        url: "/api/AddAirlline",
+        type: "POST", 
+        data: JSON.stringify(data),
+        contentType: "application/json", 
+        success: function (result) {
+            console.log("success", result);
+            window.location.href = "AirllineAdmin.html";
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log("error", xhr.responseText);
+            var errorToast = new bootstrap.Toast(document.getElementById('errorToast'));
+            errorToast.show();
+        }
+    });
+
+}
+
+function convertFormToJSON(form) {
+    const array = $(form).serializeArray(); // Encodes the set of form elements as an array of names and values.
+    const json = {};
+    $.each(array, function () {
+        json[this.name] = this.value || "";
+    });
+    return json;
 }
