f0a0907ad9a06fa63af37b6a7b3361c4c7276ba3
Dragan <milosevicdragan002@gmail.com>
2024-06-14 19:24:20 +0200

    GetAllAirlline func

---
+++ Rezervacija_Avio_Karata/Controllers/AirllineController.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Rezervacija_Avio_Karata.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Rezervacija_Avio_Karata.Controllers
+{
+    [RoutePrefix("api")]
+    public class AirllineController : ApiController
+    {
+        [HttpGet]
+        [Route("GetAllAirllines")]
+        public List<Airlline> GetAllAirllines()
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"));
+            List<Airlline> airllines = JsonConvert.DeserializeObject<List<Airlline>>(content) ?? new List<Airlline>();
+            return airllines;
+        }
+    }
+}
