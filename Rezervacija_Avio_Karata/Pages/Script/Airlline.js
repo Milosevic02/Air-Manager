31958f27521b0212228f869af6f56972b5526a90
Dragan <milosevicdragan002@gmail.com>
2024-06-18 18:02:26 +0200

    Add dynamic Airlline card

--- Rezervacija_Avio_Karata/Pages/Script/Airlline.js
+++ Rezervacija_Avio_Karata/Pages/Script/Airlline.js
@@ -1,23 +1,46 @@
-function LoadAirllines(){
+function LoadAirllines(role){
     $.get("/api/GetAllAirllines",function(data){
-        let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Address</th><th scope="col">Contact Info</th><th scope="col">Edit</th><th scope="col">Delete</th></tr></thead><tbody>';
 
-        let counter = 0;
-        for(airlline in data){
-            counter ++;
-            let row = '<td>' + counter.toString() + '</td>'; 
-            row += '<td>' + data[airlline].Name + '</td>';
-            row += '<td>' + data[airlline].Address + '</td>'; 
-            row += '<td>' + data[airlline].ContactInfo + '</td>'; 
-            row += '<td class="text-center">  <button type="button" class="btn btn-warning text-dark" data-bs-toggle="modal" data-bs-target="#editAirllineModal"><i class="fas fa-pen"></i> Edit</button></td>'; 
-            row += '<td class="text-center">   <button type="button" class="btn btn-danger text-light" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="fas fa-trash"></i> Delete</button></td>'; 
-
-            table += '<tr>' + row + '<tr/>';
+        if(role != "Admin"){
+            AirllineCards(data);
+        }else{
+            AdminTable(data);
         }
-
-        table += '</tbody></table>';
-        $('#airllineTable').html(table);
     })
+}
+
+function AirllineCards(data){
+    let card = '<div class="row">';
+    for(airlline in data){
+        card += '<div class="card p-3 sec-color text-light" style="width: 22rem;margin-bottom:50px !important;margin-left:80px !important"><div class="card-body">';
+        card += '<h5 class="card-title">'+ data[airlline].Name + '</h5><div class="form-group"><label for="address" class="sec-color text-light">Address:</label>';
+        card += '<span id="address" class="form-control sec-color text-light" readonly>' + data[airlline].Address + '</span></div>';
+        card += '<div class="form-group"><label for="contact" class="sec-color text-light">Contact Info:</label>'
+        card += '<span id="contact" class="form-control sec-color text-light" readonly>' + data[airlline].ContactInfo + '</span></div></div><div class="card-body view-btn">';
+        card += '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#airllineModal"><i class="fas fa-eye"></i> View Airline</button></div></div>'
+    }
+    card += '</div>';
+    $("#airllineCard").html(card);
+}
+
+function AdminTable(data){
+    let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Address</th><th scope="col">Contact Info</th><th scope="col">Edit</th><th scope="col">Delete</th></tr></thead><tbody>';
+
+    let counter = 0;
+    for(airlline in data){
+        counter ++;
+        let row = '<td>' + counter.toString() + '</td>'; 
+        row += '<td>' + data[airlline].Name + '</td>';
+        row += '<td>' + data[airlline].Address + '</td>'; 
+        row += '<td>' + data[airlline].ContactInfo + '</td>'; 
+        row += '<td class="text-center">  <button type="button" class="btn btn-warning text-dark" data-bs-toggle="modal" data-bs-target="#editAirllineModal"><i class="fas fa-pen"></i> Edit</button></td>'; 
+        row += '<td class="text-center">   <button type="button" class="btn btn-danger text-light" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="fas fa-trash"></i> Delete</button></td>'; 
+
+        table += '<tr>' + row + '<tr/>';
+    }
+
+    table += '</tbody></table>';
+    $('#airllineTable').html(table);
 }
 
 function AddAirlline(event){
