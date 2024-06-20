3812edcefd27ca1db203d6b83c647c6b051423e7
Dragan <milosevicdragan002@gmail.com>
2024-06-20 14:00:16 +0200

    Edit Airline func

--- Rezervacija_Avio_Karata/Controllers/AirllineController.cs
+++ Rezervacija_Avio_Karata/Controllers/AirllineController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Xml.Linq;
 
 namespace Rezervacija_Avio_Karata.Controllers
 {
@@ -54,6 +55,69 @@ namespace Rezervacija_Avio_Karata.Controllers
                 airline.Reviews = airline.Reviews.Where(r => r.ReviewStatus == ReviewStatus.Approved).ToList();
             }
             return Ok(airline);
+        }
+
+        [HttpPut]
+        [Route("EditAirline")]
+        public IHttpActionResult EditAirline(Airlline airline,string oldName)
+        {
+       
+            if (airline.Name != oldName)
+            {
+                if (IsAirlineExists(airline.Name))
+                {
+                    return BadRequest("Airline with name " + airline.Name + "already exists");
+                }
+                FileChangeAirlineForFlights(airline.Name, oldName);
+            }
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"));
+            List<Airlline> airllines = JsonConvert.DeserializeObject<List<Airlline>>(content) ?? new List<Airlline>();
+            for (int i = 0; i < airllines.Count; i++)
+            {
+                if (airllines[i].Name == oldName)
+                {
+                    airllines[i].Name = airline.Name;
+                    airllines[i].Address = airline.Address;
+                    airllines[i].ContactInfo = airline.ContactInfo;
+                    break;
+                }
+            }
+            content = JsonConvert.SerializeObject(airllines, Formatting.Indented);
+            File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"), content);
+            return Ok();
+        }
+
+
+        private bool IsAirlineExists(string AirlineName)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"));
+            List<Airlline> airllines = JsonConvert.DeserializeObject<List<Airlline>>(content) ?? new List<Airlline>();
+            foreach (Airlline air in airllines)
+            {
+                if (air.Name == AirlineName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void FileChangeAirlineForFlights(string airlineName, string oldName)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"));
+            List<Airlline> airllines = JsonConvert.DeserializeObject<List<Airlline>>(content) ?? new List<Airlline>();
+            foreach(Airlline air in airllines)
+            {
+                if(air.Name == oldName)
+                {
+                    for (int i = 0; i < air.Flights.Count; i++) {
+                        air.Flights[i].Airline = airlineName;
+                    }
+                    break;
+                }
+            }
+            content = JsonConvert.SerializeObject(airllines, Formatting.Indented);
+            File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"), content);
         }
     }
 }
