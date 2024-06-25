e46d1fe6e128329e692e11236202c52157a368e7
Dragan <milosevicdragan002@gmail.com>
2024-06-25 16:30:32 +0200

    Admin Review func

--- Rezervacija_Avio_Karata/Controllers/ReviewController.cs
+++ Rezervacija_Avio_Karata/Controllers/ReviewController.cs
@@ -58,5 +58,90 @@ namespace Rezervacija_Avio_Karata.Controllers
             }
             return false;
         }
+
+        [HttpGet]
+        [Route("GetCreatedReview")]
+        public List<Review> GetCreatedReview()
+        {
+            List<Review> res = new List<Review>();
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Reviews.txt"));
+            List<Review> reviews = JsonConvert.DeserializeObject<List<Review>>(content) ?? new List<Review>();
+            foreach(Review review in reviews)
+            {
+                if (review.ReviewStatus == ReviewStatus.Created)
+                {
+                    res.Add(review);
+                }
+            }
+            return res;
+        }
+
+        [HttpPost]
+        [Route("ChangeReviewStatus")]
+        public IHttpActionResult ChangeReservationStatus(int id, string action)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Reviews.txt"));
+            List<Review> reviews = JsonConvert.DeserializeObject<List<Review>>(content) ?? new List<Review>();
+            bool find = false;
+            foreach (Review review in reviews)
+            {
+                if (review.Id == id)
+                {
+                    find = true;
+                    if (action == "Approved")
+                    {
+                        review.ReviewStatus = ReviewStatus.Approved;
+                        LoadStatusInAirlineFile(id,review.Airline,true);
+                        break;
+                    }
+                    else if (action == "Rejected")
+                    {
+                        review.ReviewStatus = ReviewStatus.Rejected;
+                        LoadStatusInAirlineFile(id, review.Airline, false);
+
+                        break;
+                    }
+                }
+            }
+            if (!find) { return NotFound(); }
+            content = JsonConvert.SerializeObject(reviews, Formatting.Indented);
+            File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Reviews.txt"), content);
+            return Ok();
+        }
+
+        private void LoadStatusInAirlineFile(int id, string airlineName,bool approved)
+        {
+            string content = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"));
+            List<Airlline> airllines = JsonConvert.DeserializeObject<List<Airlline>>(content) ?? new List<Airlline>();
+
+            foreach(Airlline air in airllines)
+            {
+                if (air.Name == airlineName)
+                {
+                    for (int i = 0; i < air.Reviews.Count; i++)
+                    {
+                        if (air.Reviews[i].Id == id)
+                        {
+                            if (approved)
+                            {
+                                air.Reviews[i].ReviewStatus = ReviewStatus.Approved;
+
+                            }
+                            else
+                            {
+                                air.Reviews[i].ReviewStatus = ReviewStatus.Rejected;
+                            }
+                        }
+                    }
+                }
+            }
+
+            content = JsonConvert.SerializeObject(airllines, Formatting.Indented);
+            File.WriteAllText(Path.Combine(HttpRuntime.AppDomainAppPath + "App_Data/Airllines.txt"), content);
+        }
+
+
+
+
     }
 }
