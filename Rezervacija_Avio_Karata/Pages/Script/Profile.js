13d866d421c53899e58c737595bc5c5436a52962
Dragan <milosevicdragan002@gmail.com>
2024-06-26 16:47:50 +0200

    Filter users func

--- Rezervacija_Avio_Karata/Pages/Script/Profile.js
+++ Rezervacija_Avio_Karata/Pages/Script/Profile.js
@@ -15,29 +15,69 @@ function LoadProfile() {
 
 }
 
-function LoadAllUsers(){
-    $.get("/api/GetAllUsers",function(data){
-        let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Surname</th><th scope="col">Username</th><th scope="col">Email</th><th scope="col">Date Of Birth</th><th scope="col">Role</th><th scope="col">Gender</th></tr></thead><tbody>';
+function LoadUsers(){
+    var searchByName = $("#searchByName").val();
+    var searchBySurname = $("#searchBySurname").val();
+    var lowerDateOfBirth = $("#lowerDateOfBirth").val();
+    var upperDateOfBirth = $("#upperDateOfBirth").val();
+    var sortByName = $("input[name='sortByNameOption']:checked").val();
+    var sortByDate = $("input[name='sortByDateOption']:checked").val();
 
-        let counter = 0;
-        for(user in data){
-            counter++;
-            let row = '<td>' + counter.toString() + '</td>'; 
-            row += '<td>' + data[user].Name + '</td>';
-            row += '<td>' + data[user].Surname + '</td>'; 
-            row += '<td>' + data[user].Username + '</td>'; 
-            row += '<td>' + data[user].Email + '</td>'; 
-            row += '<td>' + data[user].DateOfBirth + '</td>'; 
-            row += '<td>' + data[user].Role + '</td>'; 
-            var genderValue = data[user].Gender == 0 ? "Male" : "Female";
-            row += '<td>' + genderValue + '</td>'; 
+    var filter = {
+        SearchByName: searchByName,
+        SearchBySurname: searchBySurname,
+        LowerDateOfBirth: lowerDateOfBirth,
+        UpperDateOfBirth: upperDateOfBirth,
+        SortByName: sortByName,
+        SortByDate: sortByDate
+    };
 
-            table += '<tr>' + row + '<tr/>';
+    $.ajax({
+        url: "/api/GetAllUsers",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(filter),
+        success: function (data) {
+            displayUsers(data);
+        },
+        error: function (error) {
+            console.log("Error filtering users:", error);
         }
+    });
+}
 
-        table += '</tbody></table>';
-        $('#userTable').html(table);
-    })
+function displayUsers(users) {
+    let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Surname</th><th scope="col">Username</th><th scope="col">Email</th><th scope="col">Date Of Birth</th><th scope="col">Role</th><th scope="col">Gender</th></tr></thead><tbody>';
+
+    let counter = 0;
+    users.forEach(function (user) {
+        counter++;
+        let row = '<td>' + counter.toString() + '</td>';
+        row += '<td>' + user.Name + '</td>';
+        row += '<td>' + user.Surname + '</td>';
+        row += '<td>' + user.Username + '</td>';
+        row += '<td>' + user.Email + '</td>';
+        row += '<td>' + user.DateOfBirth + '</td>';
+        row += '<td>' + user.Role + '</td>';
+        var genderValue = user.Gender == 0 ? "Male" : "Female";
+        row += '<td>' + genderValue + '</td>';
+
+        table += '<tr>' + row + '</tr>';
+    });
+
+    table += '</tbody></table>';
+    $('#userTable').html(table);
+}
+
+function Reset() {
+    $("#searchByName").val("");
+    $("#searchBySurname").val("");
+    $("#lowerDateOfBirth").val("");
+    $("#upperDateOfBirth").val("");
+    $("input[name='sortByNameOption']").prop("checked", false);
+    $("input[name='sortByDateOption']").prop("checked", false);
+
+    LoadUsers();
 }
 
 
