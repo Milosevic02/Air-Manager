86fc07b428c584959a48c4e7a84f9ab74857b6f0
Dragan <milosevicdragan002@gmail.com>
2024-06-26 11:11:51 +0200

    Add check if list review is empty

--- Rezervacija_Avio_Karata/Pages/Script/Review.js
+++ Rezervacija_Avio_Karata/Pages/Script/Review.js
@@ -1,25 +1,30 @@
 function LoadCreatedReview(){
     $.get("/api/GetCreatedReview",function(data){
-        let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">User</th><th scope="col">Airlline</th><th scope="col">Title</th><th scope="col">Image</th><th scope="col">Description</th><th scope="col">Approve</th><th scope="col">Reject</th></tr></thead><tbody>';
+        if(data.length > 0){
+            let table = '<table class="table table-striped table-hover table-bordered fs-5"><thead><tr><th scope="col">#</th><th scope="col">User</th><th scope="col">Airlline</th><th scope="col">Title</th><th scope="col">Image</th><th scope="col">Description</th><th scope="col">Approve</th><th scope="col">Reject</th></tr></thead><tbody>';
 
-        let counter = 0;
-        for(review in data){
-            counter++;
-            let row = '<td>' + counter.toString() + '</td>'; 
-            row += '<td>' + data[review].Reviewer + '</td>';
-            row += '<td>' + data[review].Airline + '</td>'; 
-            row += '<td>' + data[review].Title + '</td>'; 
-            row += '<td>' + data[review].Image + '</td>'; 
-            row += '<td>' + data[review].Description + '</td>'; 
-
-            row += '<td class="text-center">  <button onclick ="ChangeStatus(\'' + data[review].Id + '\', \'Approved\')" type="button" class="btn btn-success text-light"><i class="fas fa-check"></i></button></td>'; 
-            row += '<td class="text-center">   <button onclick="ChangeStatus(\'' + data[review].Id + '\', \'Rejected\')" type="button" class="btn btn-danger text-light"><i class="fas fa-times"></i></button></td>'; 
-
-            table += '<tr>' + row + '<tr/>';
+            let counter = 0;
+            for(review in data){
+                counter++;
+                let row = '<td>' + counter.toString() + '</td>'; 
+                row += '<td>' + data[review].Reviewer + '</td>';
+                row += '<td>' + data[review].Airline + '</td>'; 
+                row += '<td>' + data[review].Title + '</td>'; 
+                row += '<td>' + data[review].Image + '</td>'; 
+                row += '<td>' + data[review].Description + '</td>'; 
+    
+                row += '<td class="text-center">  <button onclick ="ChangeStatus(\'' + data[review].Id + '\', \'Approved\')" type="button" class="btn btn-success text-light"><i class="fas fa-check"></i></button></td>'; 
+                row += '<td class="text-center">   <button onclick="ChangeStatus(\'' + data[review].Id + '\', \'Rejected\')" type="button" class="btn btn-danger text-light"><i class="fas fa-times"></i></button></td>'; 
+    
+                table += '<tr>' + row + '<tr/>';
+            }
+    
+            table += '</tbody></table>';
+            $('#reviewTable').html(table);
+        }else{
+            $('#reviewTable').html('<h1 class="text-light">No review available.</h1>');
         }
 
-        table += '</tbody></table>';
-        $('#reviewTable').html(table);
     })
 }
 
